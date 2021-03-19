60d395b97827ea3d69d33dc4eed0eb9df8e9c2a1
ndx1905-github <ndx1905-github@yahoo.com>
2021-03-19 23:26:49 +0100

    ajout des lignes esclave

--- colordu.ino
+++ colordu.ino
@@ -1,7 +1,17 @@
-//ajouter librairie serie slave ici
 
 #include <Colorduino.h>
 #include <math.h>
+
+#include <Wire.h> // (code esclave)
+
+#define SDA_PIN 4 // (code esclave)
+#define SCL_PIN 5 // (code esclave)
+
+const int16_t I2C_MASTER = 0x42;  //(code esclave)
+const int16_t I2C_SLAVE = 0x08;   //(code esclave)
+
+  Wire.begin(SDA_PIN, SCL_PIN, I2C_SLAVE); // code esclave new syntax: join i2c bus (address required for slave)
+  Wire.onReceive(receiveEvent); // code esclave register event
 
 double latestHighTide , now ;
 int graph[] = {7,7,7,6,6,5,5,3,3,1,1,0,0,0,0,1,1,3,3,5,5,6,6,7,7,7,7,6,6,5,5,3,3,1,1,0,0,0,0,1,1,3,3,5,5,6,6,7};
@@ -40,7 +50,8 @@ void niveauMaree(unsigned char L)
 
 void setup()
 {
-  
+  Serial.begin(115200);           // start serial for output (code esclave)
+
   Colorduino.Init(); // initialize the board
   
   // compensate for relative intensity differences in R/G/B brightness
@@ -75,6 +86,18 @@ niveauMaree(maree);
   
 Colorduino.FlipPage();
 
- 
- 
+
+}
+
+// function that executes whenever data is received from master
+// this function is registered as an event, see setup() code esclave
+void receiveEvent(size_t howMany) {
+
+  (void) howMany;
+  while (1 < Wire.available()) { // loop through all but the last
+    int x = Wire.read(); // receive byte as a character
+    //Serial.print(c);         // print the character
+  }
+  int y = Wire.read();    // receive byte as an integer
+  Serial.println(x);         // print the integer
 }
